b958472992e8a0218d6ea3e2ae055e78f7d04163
Namrata Haribal <namrata@minerva.kgi.edu>
2019-08-04 16:28:00 -0400

    Updates

---
+++ precache-manifest.03866712555ea8cda6efc8473b012c90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69327c9552650298320f90131ae027f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb861635b360eeb1b777",
+    "url": "/static/css/main.1ba4f9be.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "bb861635b360eeb1b777",
+    "url": "/static/js/main.8e4a4b3e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

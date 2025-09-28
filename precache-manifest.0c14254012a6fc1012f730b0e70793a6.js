7d747c8ae8a69af8262c3a8e2022a9a0ff02853e
namrataharibal <haribalnamrata@gmail.com>
2025-09-28 16:21:23 +0100

    Updates

--- precache-manifest.0c14254012a6fc1012f730b0e70793a6.js
+++ precache-manifest.0c14254012a6fc1012f730b0e70793a6.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "779c0d4a4923fc89523b4df50b3a63f6",
+    "revision": "c3deb6c919fbcb28e95290c810c6a39c",
     "url": "/index.html"
   },
   {
-    "revision": "16a16394a1f37c2374d2",
-    "url": "/static/css/main.bc508bfc.chunk.css"
+    "revision": "fb095abf1a52a276fe5c",
+    "url": "/static/css/main.4fa21d4f.chunk.css"
   },
   {
     "revision": "fc8452304b106829cb25",
     "url": "/static/js/2.3b045380.chunk.js"
   },
   {
-    "revision": "16a16394a1f37c2374d2",
+    "revision": "fb095abf1a52a276fe5c",
     "url": "/static/js/main.c462db2d.chunk.js"
   },
   {
